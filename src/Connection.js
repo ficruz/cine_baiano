1833c058966e3504111d53e12090217d6c481f20
ficruz <ficruz@users.noreply.github.com>
2021-06-23 15:05:18 -0400

    rolling heroku server back

--- src/Connection.js
+++ src/Connection.js
@@ -1,5 +1,5 @@
-const Connection = { api: "http://18.228.46.122:80" };
+//const Connection = { api: "http://18.228.46.122:80" };
 
-//const Connection = { api: "https://cine-baiano.herokuapp.com" };
+const Connection = { api: "https://cine-baiano.herokuapp.com" };
 
 export { Connection };
