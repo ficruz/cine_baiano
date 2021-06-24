6ae92bbc29ede68bb93bfffe72c6130f449298ec
ficruz <ficruz@users.noreply.github.com>
2021-06-24 17:26:06 -0400

    change api server to EC2

--- src/Connection.js
+++ src/Connection.js
@@ -1,5 +1,9 @@
-//const Connection = { api: "http://18.229.118.205:3001" };
-const Connection = { api: "http://localhost:3001" };
+// ** EC2 connection
+const Connection = { api: "http://18.229.118.205:3001" };
+
+//const Connection = { api: "http://localhost:3001" };
+
+// ** Heroku server
 //const Connection = { api: "https://cine-baiano.herokuapp.com" };
 
 export { Connection };
